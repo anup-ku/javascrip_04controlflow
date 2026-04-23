91ccf4d2c12fd75328dabf255261d480f92bd93a
anup-ku <anupkumarpandit2104@gmail.com>
2026-04-23 11:40:58 +0530

    first commit

---
+++ 03_truthy.js
@@ -0,0 +1,50 @@
+const userEmail = "an@itee.ai"
+
+if(userEmail){
+    console.log("Got user email");
+}else{
+    console.log("Don 't hve user email");
+}
+
+
+// falsy value
+
+// false , 0, -0,BigInt 0n, "", null, undefined, NaN
+
+//truthy value 
+// "0", 'false'," ",[],{},function(){}
+
+
+
+
+// if(userEmail.length ===0){
+//     console.log("Array is empty");
+// }
+
+
+const emptyObj ={}
+
+if (Object.keys(emptyObj).length ===0){
+    console.log("object is empty");
+}
+
+
+// Nullish Coalescing Operator (??): null undefind
+
+let val1;
+// val1 = 5??10
+//val1 = null?? 10
+//val1 = undefined ??15
+
+val1 = null ?? 10 ??20
+
+
+console.log(val1);
+
+
+//Terniary Operator
+
+//condition ? true :false
+
+const iceTeaPrice =100
+iceTeaPrice <=80? console.log("less than 90"):console.log("more than 90")
